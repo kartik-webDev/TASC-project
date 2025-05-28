27a9c1985bc6e3bbd6306ea71fe1840e4f30ca07
kartik-webDev <kartik.microservices.dev@gmail.com>
2025-05-28 22:14:16 +0530

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,7 +17,8 @@ const eslintConfig = [
     "@typescript-eslint/no-unused-vars": ["error", { "varsIgnorePattern": "^_" }],
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-unused-vars": "off",
-    
+    "react/no-unescaped-entities": "off"
+
 
   }
 }
